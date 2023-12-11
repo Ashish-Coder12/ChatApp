6d01aa54ac94d125a45bc3d92887b06ccf979932
ashish garg <ashish.garg@metamerch.in>
2023-12-11 20:49:03 +0530

    copy code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import './App.css'
 import './styles/listarea.css'
 import './styles/chatPage.css'
 import { Navbar } from "./Components/Navbar";
-import { List } from './Components/list'
+import { List } from './Components/List'
 import { Chat  } from "./Components/Chat";
 import Details from './Components/Details'
 import Login from './pages/Login'
