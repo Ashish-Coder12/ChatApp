ba0cde4cd19d93d9008c3e079eb2cdaf05d01552
ashish garg <ashish.garg@metamerch.in>
2023-12-12 19:28:09 +0530

    allow all cors

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -5,15 +5,12 @@ import { cors } from "cors";
 
 const app  = express()
 
-var corsOptions = {
-  origin: 'https://picochat.vercel.app',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
   })
-app.get('/api/users', cors(corsOptions), (req, res) => {
+app.get('/api/users', (req, res) => {
     res.send(Users)
   })
 app.get('/api/pawan', cors(corsOptions),(req, res) => {
