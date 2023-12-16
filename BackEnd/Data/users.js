83547f82c4126573b76406f4a6ae888729018d95
ashish garg <ashish.garg@metamerch.in>
2023-12-16 20:15:15 +0530

    added authenticate user

---
+++ BackEnd/Data/users.js
@@ -0,0 +1,26 @@
+const Accounts  = [
+    {
+      unique_id: '1Ashish',
+      username: "Ashish",
+      name: "Ashish Garg",
+      email: "Yeshu.ya1716@gmail.com",
+      password: "12345",
+    },
+    {
+      unique_id: '2Deepu',
+      username: "Deepu",
+      name: "Deepu Prajapari",
+      email: "deepu@gmail.com",
+      password: "12345",
+    },
+    {
+      unique_id: '3Pawan',
+      username: "Pawan",
+      name: "Pawan Kulshetra",
+      email: "pawan@gmail.com",
+      password: "12345",
+    },
+
+  ];
+  
+  export {Accounts};
--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import * as Users from "./Data/data.js";
+import * as Accounts from "./Data/users.js";
 import 'dotenv/config'
 import cors from "cors";
 
 const app  = express()
 
 app.use(cors())
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -16,7 +18,23 @@ app.get('/api/users', (req, res) => {
 app.get('/api/pawan',(req, res) => {
     res.send("HII Pawan")
   })
-
+  app.post('/authenticate', (req, res) => {
+    // try {
+      
+    // } catch (error) {
+    //   res.status(500).json({ status: 'error', message: "Internal server Error" });
+    // }
+    console.log(req.body);
+      const { username, password } = req.body;
+      const data = Accounts.Accounts
+      const user = data.find(u => u.username === username && u.password === password);
+      if (user) {
+        res.status(200).json({ status: 'success', unique_id: user.unique_id });
+      } else {
+        res.status(401).json({ status: 'failed', message: 'Invalid credentials' });
+      }
+   
+  });
 
 app.listen(process.env.PORT ,()=>{
     console.log(`server is running on port : ${process.env.PORT}`);
