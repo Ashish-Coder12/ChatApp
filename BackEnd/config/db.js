550e014075a8eb0b64448cd7ea40279d441b9aa8
ashish garg <ashish.garg@metamerch.in>
2024-02-12 20:33:16 +0530

    connection formate fixed

--- BackEnd/config/db.js
+++ BackEnd/config/db.js
@@ -1,16 +1,19 @@
 import mongoose from "mongoose";
 
-const connectToDB = async() =>{
-    try {
-        const connection = await mongoose.connect(process.env.MONGO_URI,{
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            // useFindAndModify: true,
-        })
-        console.log("Mongo DB conected : " + connection.connection.host);
-    } catch (error) {
-        console.log(error);
-    }
+
+
+// Connects to the MongoDB database using the MONGO_URI environment variable
+const connectToDB = async () => {
+   try {
+       const options = {
+           useNewUrlParser: true,
+           useUnifiedTopology: true,
+       };
+       const connection = await mongoose.connect(process.env.MONGO_URI, options);
+       console.log("MongoDB connected:", connection.connection.host);
+   } catch (error) {
+       console.error(error);
+   }
 }
 
 export {connectToDB};
